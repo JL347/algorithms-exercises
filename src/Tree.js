6e3de3e3b4241257dc21c21043c834af773ef9b0
JL347 <jaredlemkedev@gmail.com>
2023-03-08 16:40:37 -0800

    added logic for trees

--- src/Tree.js
+++ src/Tree.js
@@ -4,18 +4,57 @@ import { TreeViz } from "./tree-visualizer";
 import _ from "lodash";
 
 class Tree {
-  add(num) {
-    // needs this
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    if (this.root === null) {
+      this.root = new Node(value);
+    } else {
+      let current = this.root;
+      while (true) {
+        if (current.value > value) {
+          // go left
+
+          if (current.left) {
+            current = current.left;
+          } else {
+            current.left = new Node(value);
+            break;
+          }
+        } else {
+          // go right
+          if (current.right) {
+            current = current.right;
+          } else {
+            current.right = new Node(value);
+            break;
+          }
+        }
+      }
+    }
+    return this;
+  }
+  toJSON() {
+    return JSON.stringify(this.root.serialize(), null, 4);
   }
   toObject() {
-    // returns a root node with
-    // {
-    //   value:  <number>
-    //   left:   <object>
-    //   right:  <object>
-    //   height: <number, optional>
-    // }
-    return { value: 1, left: null, right: null, height: 1 };
+    return this.root.serialize();
+  }
+}
+
+class Node {
+  constructor(value = null, left = null, right = null) {
+    this.left = left;
+    this.right = right;
+    this.value = value;
+  }
+  serialize() {
+    const ans = { value: this.value };
+    ans.left = this.left === null ? null : this.left.serialize();
+    ans.right = this.right === null ? null : this.right.serialize();
+    return ans;
   }
 }
 
